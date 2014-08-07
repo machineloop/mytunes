cd6d480f1e766a542ce0d5d89c4073f079447b6c
jabbrass <jabbrass@zoho.com>
2014-08-06 18:21:08 -0700

    Started SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,13 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    this.on('add', this.playFirst);
+  },
+
+  playFirst: function() {
+    if (this.length === 1) {
+      this.models[0].play();
+    }
+  },
 
 });
